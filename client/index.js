8aa9367c45477c8ea716af7f337cbd28d7d253d3
ceelo777 <lochristopher9@gmail.com>
2020-10-07 19:43:14 -0400

    Finished functionality for joining BlueJeans workshops

--- client/index.js
+++ client/index.js
@@ -254,7 +254,7 @@ define([
 
 
 		// Meeting UI handlers
-		$("#joinMeeting").click(function() {
+		$("#joinMeeting").click(function() {			
 			var meetingParams = {
 				numericMeetingId   : $('#id').val(),
 				attendeePasscode    : $('#passCode').val(),
@@ -274,3 +274,68 @@ define([
 	maptoUI();
 
 });
+
+var openWorkshops = document.getElementById("open-workshops");
+var curr = document.getElementById("workshops-list");
+openWorkshops.addEventListener("click", function() {	
+	if (curr.style.display == "") {
+		curr.style.display = "none";
+	} else {
+		curr.style.display = "";
+	}
+});
+
+workshops = [
+	{
+		Name: "Intro to React",
+		Points: 600,		
+		ID: 8196619129,
+		Pass: 2177
+	},
+	{
+		Name: "Get to Know NCR",
+		Points: 400
+	},
+	{
+		Name: "Yoga with Microsoft",
+		Points: 300
+	},
+	{
+		Name: "Bob Ross Painting",
+		Points: 400
+	}
+]
+
+for (let i = 0; i < workshops.length; i++) {
+	var elem = document.createElement('div');
+
+	var elemName = document.createElement('div');
+	elemName.classList.add("workshop-name")
+
+	var elemPoints = document.createElement('div');
+	elemPoints.classList.add("workshop-points");
+
+	elemName.innerHTML = workshops[i]['Name'];
+	elemPoints.innerHTML = workshops[i]['Points'] + " Points";
+
+	var idName = "workshop-" + (i + 1);	
+
+	elem.classList.add("workshop-elem");
+	elem.id = idName;
+	elem.append(elemName);
+	elem.append(elemPoints);	
+	curr.append(elem);
+}
+
+document.getElementById("workshops-list")
+	.querySelectorAll(".workshop-elem")
+	.forEach(item => item.addEventListener("click", function(event) {
+		var name = event.target.innerHTML;
+		for (let i = 0; i < workshops.length; i++) {
+			if (workshops[i]["Name"] == name) {
+				document.getElementById("id").value = workshops[i]["ID"];
+				document.getElementById("passCode").value = workshops[i]["Pass"];				
+				break;
+			}
+		}
+}));
