dbfd6ea5722aa95aadd31b53092847906032647b
mohandodda0 <mohandodda@rocketmail.com>
2020-10-13 14:51:53 -0500

    user:null from graphql

--- server/src/graphqlrouter.ts
+++ server/src/graphqlrouter.ts
@@ -3,11 +3,10 @@ import path from "path";
 import { buildSchema } from "graphql"
 import { IUser, User, Event } from "./schema";
 var express = require('express')
-var apiRouter = express.Router()
 const bodyParser = require('body-parser')
 const express_graphql = require("express-graphql")
 const typeDefs = fs.readFileSync(path.resolve(__dirname, "../api.graphql"), "utf8");
-
+var apiRouter = express.Router()
 let getUser = async function (parent, args, context, info) {
     // if (!context._id) {
     //     throw new Error('User not logged in')
@@ -26,7 +25,9 @@ let getUser = async function (parent, args, context, info) {
     // return user;
     // return user
 }
-apiRouter.use(bodyParser.text({ type: 'application/graphql' }));
+// apiRouter.use(bodyParser.text({ type: 'application/graphql' }));
+apiRouter.use(/\/((?!graphql).)*/, bodyParser.urlencoded({ extended: true }));
+apiRouter.use(/\/((?!graphql).)*/, bodyParser.json());
 const root = {
     user: getUser,
     // completed: getCompleted,
