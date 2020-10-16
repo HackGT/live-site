c65ed9e091f05e6c8d13b171801f43c44137cac4
Stephanie <almeidasmail@gmail.com>
2020-10-16 10:33:14 -0400

    Add function to let other apps access graphql endpoints

--- participant_server/src/app.ts
+++ participant_server/src/app.ts
@@ -177,8 +177,34 @@ app.get('/dashboard', (req, res) => {
 //     graphiql:process.env.ISPRODUCTION !== 'true'
 // }))
 
+function isAdmin(request: express.Request, response: express.Response, next: express.NextFunction) {
+    response.setHeader("Cache-Control", "private");
+	let user = request.user as IUser;
+	const auth = request.headers.authorization;
+
+	if (auth && typeof auth === "string" && auth.indexOf(" ") > -1) {
+		const key = Buffer.from(auth.split(" ")[1], "base64").toString();
+		if (key === process.env.SECRET) {
+			next();
+		}
+		else {
+			response.status(401).json({
+				"error": "Incorrect auth token!"
+			});
+		}
+	}
+	else if (!request.isAuthenticated()) {
+		response.status(401).json({
+			"error": "You must log in to access this endpoint"
+		});
+	}
+	else {
+		next();
+	}
+}
+
 var apigraphql = require('./graphqlrouter')
-app.use('/graphql', isAuthenticated, apigraphql)
+app.use('/graphql', isAdmin, apigraphql)
 
 
 
