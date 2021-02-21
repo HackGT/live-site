aae586b45493e5fe1c6bc8d9d519ae3841d3258f
johnwashburne <johnwashburne@outlook.com>
2021-02-21 17:59:38 -0500

    working websockets

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,14 +3,16 @@ import path from "path";
 import express from "express";
 import compression from "compression";
 import morgan from "morgan";
-import cors from "cors"
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
+import {Server} from "ws";
 
 dotenv.config();
 
 const VERSION_NUMBER = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf8")).version;
 const PORT = process.env.PORT || 3000;
 export let app = express();
+
 
 app.use(morgan("dev"));
 app.use(compression());
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
     next();
   });
 
+
 // Throw and show a stack trace on an unhandled Promise rejection instead of logging an unhelpful warning
 process.on("unhandledRejection", err => {
     throw err;
@@ -43,23 +46,9 @@ app.get("/status", (req, res) => {
     res.status(200).send("Success");
 });
 
-
-// app.get("/", (req, res) => {
-//     res.redirect("https://live.hack.gt");
-// });
-
 app.use("/auth", authRoutes);
 app.use("/event", isAuthenticated, eventRoutes);
 app.use("/user", userRoutes);
-
-// app.get("*", (req, res) => {
-//     res.status(404).send("Sorry :( this is an invalid url");
-// })
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`Virtual Check-in system v${VERSION_NUMBER} started on port ${process.env.PORT}`);
-// });
-
 
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "../../participant", "index.html"));
@@ -81,27 +70,25 @@ app.get("/*", function (req, res) {
     );
 });
 
-app.post('/times', function (req, res) {
-    const u = req.user as IUser;
-    const t = req.body.time;
-    const time1: ITime = new Time({
-        time: t,
-        user_id: u.uuid
-    })
-    time1.save().then(() => console.log('done'));
-    res.send("success")
-});
-
-app.post('/timesreact', function (req, res) {const u = req.user as IUser;
-    const t = req.body.time;
-    const time1: ITime = new Time({
-        time: t,
-    })
-    time1.save().then(() => console.log('done'));
-    res.status(200).send("success");
-});
-
-
 app.listen(PORT, () => {
     console.log(`Virtual Check-in system v${VERSION_NUMBER} started on port ${PORT}`);
 });
+
+const wss = new Server({server: app, port: 8080});
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    const start = Date.now();
+    let delta;
+    setInterval(function() {
+        delta = Math.floor((Date.now() - start) / 1000);
+    }, 1000);
+    ws.on('close', () => {
+        const time1: ITime = new Time({
+            time: delta,
+        });
+        time1.save().then(() => console.log('websocket time saved'));
+        console.log('Client disconnected')
+    });
+});
+
+app.disable('etag');
