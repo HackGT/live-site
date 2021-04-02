b3d78e96c339c2e5637e52c4b12772c52f5cc77a
johnwashburne <johnwashburne@outlook.com>
2021-04-01 21:03:59 -0400

    meeting commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import VideoWindow from './components/VideoWindow';
 import logo from './assets/logo.png';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 const App: React.FC = () => {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="HackGT-logo"/>
         <Router>
           <Switch>
-            <Route path="/events/:id" children={<VideoWindow/>} />
+            <Route path="/:id" children={<VideoWindow/>} />
             <Route path="/" children={<h1>Something went wrong, please try again or contact HackGT staff!</h1>} />
           </Switch>
         </Router>
