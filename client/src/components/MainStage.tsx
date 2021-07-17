f8cbdbb3f409c4e931e2cdb23240253d46543dd4
Thomas Lang <langthomas.tl@gmail.com>
2021-07-17 18:30:52 -0400

    Remove old files and start main page

---
+++ client/src/components/MainStage.tsx
@@ -0,0 +1,37 @@
+import '../App.css';
+
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardTag from './CardTag'
+
+const MainStage: React.FC = () => {
+  return (
+    <div>
+      <div className="main_stage_container">
+        <div className="main_stage_placeholder"></div>
+        <Card className="main_stage_content">
+          <CardActionArea>
+            <CardContent>
+              <Typography align='left' gutterBottom variant="h5" component="h2">
+                Title goes here
+              </Typography>
+              <Typography align='left' variant="body2" color="textSecondary" component="p">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <CardTag tag="Standard Tag"/>
+            <CardTag tag="Standard Tag"/>
+            <CardTag tag="Standard Tag"/>
+          </CardActions>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default MainStage;
