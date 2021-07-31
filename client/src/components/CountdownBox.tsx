6877326eb67c23a7d55a9eeea0e7cbb6636229c9
Sneha <sneharoy@Snehas-MacBook-Pro.local>
2021-07-31 12:12:30 -0700

    added in the graphql query to get the event data info

--- client/src/components/CountdownBox.tsx
+++ client/src/components/CountdownBox.tsx
@@ -5,6 +5,33 @@ type Props = {
 };
 
 const CountdownBox: React.FC<Props> = (props: Props) => {
+  // var query =  
+  //   `{
+  //     allEvents  (where: {AND:[
+  //         {startDate_gt: "2021-03-13T00:00:00.000Z"},
+  //         {endDate_lt: "2021-03-14T21:00:00.000Z"}
+  //       ]}, orderBy:"startDate") {
+  //       name
+  //       startDate
+  //       url
+  //     }
+  //   }
+  //   `;
+
+  // async function fetchEvents(query: string) {
+  //   var res = await fetch("https://cms.hack.gt/admin/api", {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({ query: query }),
+  //   });
+  //   var jsonResponse = await res.json();
+  //   return jsonResponse.data;
+  // }
+  
+  // fetchEvents(query).then(data => {
+  //   console.log(data.allEvents)
+  // })
+
   return (
     <div>
       <div className="CountdownBox">
