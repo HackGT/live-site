a51ce113ea782dd0045e78926be11d370507dae7
Sneha Roy <sneharoy321@gmail.com>
2021-08-22 12:51:43 -0400

    added in the changes for daily

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { User } from "../schema";
+import { IUser, User } from "../schema";
 import { isAdmin } from "../auth/auth";
 
 export let userRoutes = express.Router();
@@ -53,6 +53,25 @@ userRoutes.route("/events/:userId").get(async (req, res) => {
     return res.send({ events: user.events });
 });
 
+userRoutes.route("/updateEnd").post(async (req, res) => {
+    let data = req.body;
+    const reqUser = req.user as IUser;
+    const user = await User.findById(reqUser._id);
+
+    if(user != null) {
+        let events = user?.events;
+        for(var i = 0; i < events.length; i++) {
+            if(events[i].id  === data.EventID) {
+                events[i].attended[events[i].attended.length - 1].exit = data.endDate;
+                await user.save(err => console.log(err));
+                // console.log(events[i].attended[events[i].attended.length - 1])
+            } 
+        }
+    }
+
+
+    return res.status(200).send("updated the end time");
+}) 
 
 //     if (!data.userId || !data.event) {
 //         return res.status(400).send({ error: true, message: "Invalid request" });
