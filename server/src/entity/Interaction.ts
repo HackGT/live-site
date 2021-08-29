0b5967b48dd89f54d9a634117b9e1a18a7d86be9
Anish Thite <anishthite@gmail.com>
2021-08-29 03:00:04 -0400

    add new schema

---
+++ server/src/entity/Interaction.ts
@@ -0,0 +1,36 @@
+import { mongoose, RootDocument } from './database'
+//single interaction instance
+export interface IInteractionInstance {
+    timeIn: Date,
+    timeOut: Date | undefined,
+    type: string //virtual or inperson
+}
+
+
+const InteractionInstanceSchema = {
+    timein: {type: Date, required: true},
+    timeout: Date
+}
+
+//unique interaction for each event/user combo
+export interface IInteraction extends RootDocument {
+    uuid: string;
+    eventId: string;
+    timeInOutPairs?: IInteractionInstance[];
+}
+
+export const Interaction = mongoose.model<IInteraction & mongoose.Document>("Interaction", new mongoose.Schema({
+    uuid: {
+        type: String,
+        required: true,
+        index: true
+    },
+    eventID: {
+        type: String,
+        required: true,
+        index: true
+    },
+    instances: [InteractionInstanceSchema]
+}))
+
+
