6feaffdc618854e54ede16393b10ab611e77e0dc
bopas2 <langthomas.tl@gmail.com>
2021-09-18 22:36:27 -0400

    Integrated daily into frontend

---
+++ client/src/components/VideoInformation.tsx
@@ -0,0 +1,15 @@
+class VideoInformation {
+    type: string;
+    url: string;
+    status: string;
+    eventName: string;
+
+    constructor(type: string, url: string, status:string, eventName: string) {
+        this.type = type;
+        this.url = url;
+        this.status = status;
+        this.eventName = eventName;
+    }
+}
+
+export default VideoInformation
