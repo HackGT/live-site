85d088eb6ebcfaa73ad208b50c4c140e021b7e38
Mohan Dodda <42725782+mohandodda0@users.noreply.github.com>
2021-09-26 09:03:31 -0400

    Update event.ts
    query uuid by uuid field not regenerated _id

--- server/src/routes/event.ts
+++ server/src/routes/event.ts
@@ -144,7 +144,7 @@ eventRoutes.route("/virtualInteraction/:getEventID").get(async (req, res) => {
             }
         } else {
             interaction = createNew(Interaction, {
-                uuid: reqUser._id.toHexString(),
+                uuid: user.uuid,
                 eventID: event.id,
                 instances: [{
                     timeIn: now.toDate(),
@@ -185,7 +185,7 @@ eventRoutes.route("/virtualInteraction/:getEventID").get(async (req, res) => {
                   'Content-Type': 'application/json',
                   Authorization: 'Bearer ' + String(process.env.DAILY_KEY)
                 },
-                body: JSON.stringify({properties: {room_name: room_name, is_owner: false, user_name: reqUser.name, user_id: reqUser._id.toHexString()}})
+                body: JSON.stringify({properties: {room_name: room_name, is_owner: false, user_name: reqUser.name, user_id: user.uuid}})
               };
               const response = await fetch(fetch_url, options)
                 .then(res => res.json())
