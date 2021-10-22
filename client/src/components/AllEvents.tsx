fc5eab19a07b5a09564ddea13b3c4ea4d27d9167
Mohan Dodda <mohandodda@rocketmail.com>
2021-10-22 12:57:52 -0400

    allevents false

--- client/src/components/AllEvents.tsx
+++ client/src/components/AllEvents.tsx
@@ -36,7 +36,7 @@ const AllEvents: React.FC<Props> = (props: Props) => {
   useEffect(() => {
 
     const getEvents = async () => {
-      const data = await fetchAllEvents(false)
+      const data = await fetchAllEvents(true)
       const events = data.allEvents
       setEvents(events);
       set_filtered_events(events.slice(0, 6))
