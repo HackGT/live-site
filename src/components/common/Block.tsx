06c8f0ddd4a32649f7534f9c74cfd2966ac72255
Roshan Desai <rohod04@gmail.com>
2023-03-08 14:22:44 -0500

    Add edit block button to Block component

--- src/components/common/Block.tsx
+++ src/components/common/Block.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Stack } from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+import { apiUrl, Service, useAuth } from "@hex-labs/core";
+import axios from "axios";
 
 import styles from "./markdown_styles.module.css";
 
@@ -9,17 +12,52 @@ type Props = {
   block: any;
 };
 
-const Block: React.FC<Props> = (props: Props) => (
-  <Box bg="white" textAlign="left" display="flex">
-    <Box>
-      <Box color="black" fontSize="24px" fontWeight="bold">
-        {props.block.title}
-      </Box>
+const Block: React.FC<Props> = (props: Props) => {
+  const { user } = useAuth();
+
+  const [role, setRoles] = React.useState<any>({
+    member: false,
+    exec: false,
+    admin: false,
+  });
+  
+  React.useEffect(() => {
+    const getRoles = async () => {
+      if (user?.uid) {
+        const response = await axios.get(apiUrl(Service.USERS, `/users/${user?.uid}`));
+        setRoles({ ...response.data.roles });
+      }
+    };
+    getRoles();
+  }, [user?.uid]);
+  
+  const showAdmin = role.member || role.admin || role.exec;
+
+  const editBlock = () => {
+    window.location.href = `./admin/blocks/${props.block.id}`
+  }
+
+  return (
+    <Stack direction="column" bg="white" textAlign="left" display="flex" width="full">
+      <Stack direction="row" justify="space-between" width="100%">
+        <Box color="black" fontSize="24px" fontWeight="bold">
+          {props.block.title}
+        </Box>
+        { showAdmin && 
+          <Button
+            onClick={editBlock}
+            size="sm"
+            backgroundColor="red.200"
+          >
+            <EditIcon />
+          </Button>
+        }
+      </Stack>
       <ReactMarkdown className={styles.reactMarkDown} remarkPlugins={[remarkGfm]}>
         {props.block.content}
       </ReactMarkdown>
-    </Box>
-  </Box>
-);
+    </Stack>
+  )
+};
 
 export default Block;
