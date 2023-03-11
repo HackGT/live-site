dbe30b3e4c0e10cddc1c137faf4f8f449d848f4c
Thaneesh Babu <gkthaneesh2@gmail.com>
2023-03-11 00:49:32 -0500

    fix up quick links block

--- src/components/common/Block.tsx
+++ src/components/common/Block.tsx
@@ -5,12 +5,24 @@ import { Box, Button, Stack } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import { apiUrl, Service, useAuth } from "@hex-labs/core";
 import axios from "axios";
-
 import styles from "./markdown_styles.module.css";
+import LinksNav from "./LinksNav";
 
 type Props = {
   block: any;
 };
+
+const links = [
+  [
+    "Event Slack",
+    "https://join.slack.com/t/horizons-xux6929/shared_invite/zt-1qhc3lyj7-6CeeTEzvI79kL8~EeTgypQ",
+  ],
+  ["Event App (iOS)", "https://apps.apple.com/us/app/hexlabs/id1478268737"],
+  ["Event App (Android)", "https://play.google.com/store/apps/details?id=com.appgt&hl=en&gl=US"],
+  ["Horizons Website", "https://horizons.hexlabs.org/"],
+  ["Devpost", "https://horizons-2023.devpost.com/"],
+  ["Event Packet", "https://www.notion.so/b3040e44a9334c899d93403042745dda"],
+];
 
 const Block: React.FC<Props> = (props: Props) => {
   const { user } = useAuth();
@@ -20,7 +32,7 @@ const Block: React.FC<Props> = (props: Props) => {
     exec: false,
     admin: false,
   });
-  
+
   React.useEffect(() => {
     const getRoles = async () => {
       if (user?.uid) {
@@ -30,12 +42,12 @@ const Block: React.FC<Props> = (props: Props) => {
     };
     getRoles();
   }, [user?.uid]);
-  
+
   const showAdmin = role.member || role.admin || role.exec;
 
   const editBlock = () => {
-    window.location.href = `./admin/blocks/${props.block.id}`
-  }
+    window.location.href = `./admin/blocks/${props.block.id}`;
+  };
 
   return (
     <Stack direction="column" bg="white" textAlign="left" display="flex" width="full">
@@ -43,21 +55,21 @@ const Block: React.FC<Props> = (props: Props) => {
         <Box color="black" fontSize="24px" fontWeight="bold">
           {props.block.title}
         </Box>
-        { showAdmin && 
-          <Button
-            onClick={editBlock}
-            size="sm"
-            backgroundColor="red.200"
-          >
+        {showAdmin && props.block.title !== "Quick Links" && (
+          <Button onClick={editBlock} size="sm" backgroundColor="red.200">
             <EditIcon />
           </Button>
-        }
+        )}
       </Stack>
-      <ReactMarkdown className={styles.reactMarkDown} remarkPlugins={[remarkGfm]}>
-        {props.block.content}
-      </ReactMarkdown>
+      {props.block.title === "Quick Links" ? (
+        <LinksNav links={links} />
+      ) : (
+        <ReactMarkdown className={styles.reactMarkDown} remarkPlugins={[remarkGfm]}>
+          {props.block.content}
+        </ReactMarkdown>
+      )}
     </Stack>
-  )
+  );
 };
 
 export default Block;
