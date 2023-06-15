5ffbbff3be86be404d423a9bee213d7414e47979
AishwaryaC26 <67703406+AishwaryaC26@users.noreply.github.com>
2023-06-14 21:09:53 -0400

    replaced wrong app key

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ setPersistence(getAuth(app), inMemoryPersistence);
 axios.defaults.withCredentials = true;
 
 export async function runOneSignal() {
-  await OneSignal.init({ appId: '83386926-da8c-47ac-95e2-694f5e13e903' });
+  await OneSignal.init({ appId: 'cd086e3e-0229-49b9-9cde-bfc98fb3fccb3' });
   OneSignal.showSlidedownPrompt();
 }
 
