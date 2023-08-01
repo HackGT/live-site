faa0be7dc6e8d41a1259afd74fe97c6920e705f0
doddapranit <139923521+doddap123@users.noreply.github.com>
2023-08-01 16:40:54 -0400

    Hexathon Countdown Timer
    Create hexathon countdown timer without right align

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, chakra } from "@chakra-ui/react";
+import { Link, chakra, Text } from "@chakra-ui/react";
 import { Header, HeaderItem, useAuth, Service, apiUrl } from "@hex-labs/core";
 import axios from "axios";
 
@@ -22,6 +22,57 @@ const Navbar: React.FC = () => {
     exec: false,
     admin: false,
   });
+  const countdownTimerStyle = {
+    fontFamily: "monospace",
+    padding: "5px",
+    border: "2px solid #333",
+    borderRadius: "5px",
+    backgroundColor: "#f0f0f0",
+  };
+  const hexathonNameStyle = {
+    fontSize: "20px", // You can adjust the font size as desired
+    fontWeight: "bold",
+    color: "#8a2be2", // You can use any color you prefer
+  };
+
+  const [hexathons, setHexathons] = React.useState<any[]>([]);
+
+  const calculateTimeRemaining = (endTime: string): any => {
+    const updateInterval = 1000; // Update every 1 second (1000 milliseconds)
+    const endDateTime = new Date(endTime).getTime();
+  
+    const updateTimer = () => {
+      const now = new Date().getTime();
+      const timeRemaining = endDateTime - now;
+  
+      // Calculate days, hours, minutes, and seconds
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  
+      if (timeRemaining <= 0) {
+        return "Complete";
+      }
+
+      return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    };
+  
+    // Initial update
+    let timeRemaining = updateTimer();
+    const remainingTimeElement = document.getElementById("remaining-time"); // Replace "remaining-time" with the ID of the element to display the remaining time
+    if (remainingTimeElement) {
+      remainingTimeElement.textContent = timeRemaining;
+    }
+  
+    // Periodically update the timer
+    setInterval(() => {
+      timeRemaining = updateTimer();
+      if (remainingTimeElement) {
+        remainingTimeElement.textContent = timeRemaining;
+      }
+    }, updateInterval);
+  };
 
   React.useEffect(() => {
     const getRoles = async () => {
@@ -32,6 +83,27 @@ const Navbar: React.FC = () => {
     };
     getRoles();
   }, [user?.uid]);
+
+  React.useEffect(() => {
+    const getHexathons = async () => {
+      try {
+        const response = await axios.get(apiUrl(Service.HEXATHONS, '/hexathons'));
+        setHexathons(response.data);
+      } catch (error) {
+        console.error("Error fetching hexathons:", error);
+      }
+    };
+    getHexathons();
+    console.log(hexathons)
+  }, []);
+
+  const activeHexathons = hexathons.filter((hexathon: any) => {
+    const now = new Date().getTime();
+    const startDate = new Date(hexathon.startDate).getTime();
+    const endDate = new Date(hexathon.endDate).getTime();
+    console.log(hexathon)
+    return hexathon.isActive && now >= startDate && now <= endDate;
+  });
 
   const showAdmin = role.member || role.admin || role.exec;
 
@@ -56,6 +128,14 @@ const Navbar: React.FC = () => {
           <HeaderItem>Admin</HeaderItem>
         </ChakraLink>
       )}
+      {activeHexathons.map((hexathon: any) => (
+      <div key={hexathon.id}>
+        <Text style={hexathonNameStyle}>{hexathon.name}</Text>
+        <Text>
+          <span id="remaining-time" style={countdownTimerStyle}>{calculateTimeRemaining(hexathon.endDate)}</span>
+        </Text>
+      </div>
+      ))}
     </Header>
   );
 };
