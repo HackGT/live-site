328fdf84a91a71812e0f8bc5daa04d64620f83b6
Roshan Desai <rohod04@gmail.com>
2023-08-14 15:15:39 -0700

    add display field to block form

--- src/components/tabs/admin/blocks/FormInputs/BlockFormInput.tsx
+++ src/components/tabs/admin/blocks/FormInputs/BlockFormInput.tsx
@@ -6,21 +6,22 @@ import {
   FormLabel,
   HStack,
   Input,
+  Select,
   Spacer,
   Stack,
   Textarea,
   useToast,
-  VStack
+  VStack,
 } from "@chakra-ui/react";
 import { apiUrl, ErrorScreen, Service } from "@hex-labs/core";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import { blockResolver } from "../Resolvers";
-import { BlockFormValues } from "../FormValues";
+import { BlockFormValues, DisplayType } from "../FormValues";
 import styles from "../../../../common/markdown_styles.module.css";
 
 interface Props {
@@ -28,17 +29,11 @@ interface Props {
   onClose?: () => void;
 }
 
-const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
+const BlockFormInput: React.FC<Props> = ({ id, onClose }) => {
   const navigate = useNavigate();
   const [error, setError] = useState();
-  const [errors, setErrors] = useState<{[field: string]: any}>({});
-  const {
-    register,
-    reset,
-    watch,
-    getValues,
-    setValue,
-  } = useForm<BlockFormValues>({
+  const [errors, setErrors] = useState<{ [field: string]: any }>({});
+  const { register, reset, watch, getValues, setValue } = useForm<BlockFormValues>({
     resolver: blockResolver,
   });
   const toast = useToast();
@@ -46,76 +41,96 @@ const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
   const editBoxHeight = "420px";
 
   useEffect(() => {
-    const getData = async() => {
+    const getData = async () => {
       try {
-       
         const res = await axios.get(apiUrl(Service.HEXATHONS, "/blocks"));
         const data = res.data.filter((entry: any) => entry.id === id)[0];
 
         if (data) {
-            reset({...data})
+          reset({ ...data });
         }
-      } catch(e: any) {
+      } catch (e: any) {
         setError(e);
       }
-    }
+    };
     getData();
   }, [id, reset]);
-  
+
   const title = watch("title");
   const content = watch("content");
   const slug = watch("slug");
-  
+  const display = watch("display");
+
   const findMissingField = (data: any) => {
     const missingRequiredFieldError = {
-      title: (!data.title || data.title.trim().length === 0) ? ({
-        type: "required",
-        message: "Block title is required."
-      }) : undefined,
-      slug: (!data.slug|| data.slug.length === 0) ? ({
-        type: "required",
-        message: "Slug cannot be empty."
-      }) : undefined,
-      content: (!data.content || data.content.length === 0) ? ({
-        type: "required",
-        message: "Content cannot be empty."
-      }) : undefined,
-    }
+      title:
+        !data.title || data.title.trim().length === 0
+          ? {
+              type: "required",
+              message: "Block title is required.",
+            }
+          : undefined,
+      slug:
+        !data.slug || data.slug.length === 0
+          ? {
+              type: "required",
+              message: "Slug cannot be empty.",
+            }
+          : undefined,
+      content:
+        !data.content || data.content.length === 0
+          ? {
+              type: "required",
+              message: "Content cannot be empty.",
+            }
+          : undefined,
+      display:
+        !data.display || data.display.length === 0
+          ? {
+              type: "required",
+              message: "Display cannot be empty.",
+            }
+          : undefined,
+    };
 
-    const missingRequired = 
-       (!data.title || data.title.trim().length === 0)
-      || (!data.slug|| data.slug.length === 0)
-      || (!data.content || data.content.length === 0)
+    const missingRequired =
+      !data.title ||
+      data.title.trim().length === 0 ||
+      !data.slug ||
+      data.slug.length === 0 ||
+      !data.content ||
+      data.content.length === 0 ||
+      !data.display ||
+      data.display.length === 0;
 
     if (missingRequired) {
-        
-      setErrors({...missingRequiredFieldError});
+      setErrors({ ...missingRequiredFieldError });
       return 0;
     }
     return 1;
-  }
+  };
 
-  const submit = async (data: any) => {   
-    const payload: {[name: string]: any} = {
+  const submit = async (data: any) => {
+    const payload: { [name: string]: any } = {
       hexathon: String(process.env.REACT_APP_HEXATHON_ID),
       title: data.title.trim(),
       slug: data.slug.trim(),
       content: data.content,
-    }
+      display: data.display,
+    };
 
     let res = null;
     try {
       if (id) {
-        res = await axios.patch(apiUrl(Service.HEXATHONS, `/blocks/${id}`), payload)
+        res = await axios.patch(apiUrl(Service.HEXATHONS, `/blocks/${id}`), payload);
       } else {
         res = await axios.post(apiUrl(Service.HEXATHONS, "/blocks"), payload);
       }
-      
+
       if (res.status >= 200) {
         if (onClose) {
           onClose();
-        }
-        else navigate(-1);
+        } else navigate(-1);
         toast({
           title: "Success",
           description: "Event saved successfully!",
@@ -124,16 +139,15 @@ const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
           isClosable: true,
         });
       }
-    } catch(e: any) {
-      
-      setErrors({ request: JSON.parse(e.request.response) })
+    } catch (e: any) {
+      setErrors({ request: JSON.parse(e.request.response) });
     }
-  }
+  };
 
   const cancel = () => {
     if (onClose) onClose();
     else navigate(-1);
-  }
+  };
 
   const del = async () => {
     const res = await axios.delete(apiUrl(Service.HEXATHONS, `/blocks/${id}`));
@@ -148,7 +162,7 @@ const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
       duration: 5000,
       isClosable: true,
     });
-  }
+  };
 
   if (error) {
     return <ErrorScreen error={error} />;
@@ -156,89 +170,86 @@ const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
 
   return (
     <form>
-      <Stack
-        width={{ base: "100%", md: "100%" }}
-      >
+      <Stack width={{ base: "100%", md: "100%" }}>
         <Box>
-          <FormControl
-            isInvalid={errors.title}
-            marginBottom={errors.title ? "4px" : "8px"}
-          >
+          <FormControl isInvalid={errors.title} marginBottom={errors.title ? "4px" : "8px"}>
             <FormLabel>Title</FormLabel>
             <Input
-              id='title'
+              id="title"
               placeholder="Block Title"
               {...register("title")}
-              onChange={(e) => {
+              onChange={e => {
                 if (e.target.value.length <= 50 || e.target.value.length <= title.length)
                   setValue("title", e.target.value);
               }}
               value={title}
             />
-            <Box
-              width="98%"
-              textAlign="right"
-              fontSize="12px"
-              position="absolute"
-              color="#B3B3B3"
-            >
+            <Box width="98%" textAlign="right" fontSize="12px" position="absolute" color="#B3B3B3">
               {title?.length}/50 character limit
             </Box>
             <Box marginTop="2px" color="red">
-              {(errors.title ? errors.title.message : null)}
+              {errors.title ? errors.title.message : null}
             </Box>
           </FormControl>
-          <FormControl
-            isInvalid={errors.slug}
-            marginBottom={errors.slug ? "4px" : "12px"}
-          >
+          <FormControl isInvalid={errors.slug} marginBottom={errors.slug ? "4px" : "12px"}>
             <FormLabel>Slug</FormLabel>
             <Input
-              id='slug'
+              id="slug"
               placeholder="Write slug for the block"
               {...register("slug")}
-              onChange={(e) => {
+              onChange={e => {
                 if (e.target.value.length <= 200 || e.target.value.length <= slug.length)
                   setValue("slug", e.target.value);
               }}
               value={slug}
             />
-            <Box
-              width="98%"
-              textAlign="right"
-              fontSize="12px"
-              position="absolute"
-              color="#B3B3B3"
-            >
+            <Box width="98%" textAlign="right" fontSize="12px" position="absolute" color="#B3B3B3">
               {slug?.length}/50 character limit
             </Box>
             <Box marginTop="2px" color="red">
               {errors.slug && errors.slug.message}
             </Box>
           </FormControl>
+          <FormControl isInvalid={errors.display} marginBottom={errors.display ? "4px" : "12px"}>
+            <FormLabel>Display</FormLabel>
+            <Select
+              id="display"
+              placeholder="Select an option"
+              {...register("display")}
+              onChange={(e: any) => {
+                if (e.target.value.length <= 200 || e.target.value.length <= display.length)
+                  setValue("display", e.target.value);
+              }}
+              value={display}
+            >
+              {(Object.keys(DisplayType) as Array<DisplayType>).map((displayType) => (
+                <option key={displayType} value={displayType}>
+                  {displayType}
+                </option>
+              ))}
+            </Select>
+            <Box marginTop="2px" color="red">
+              {errors.display && errors.display.message}
+            </Box>
+          </FormControl>
         </Box>
         <Stack direction="row" width="full">
           <VStack width="full">
-            <FormControl
-              isInvalid={errors.content}
-              marginBottom={errors.content ? "4px" : "0px"}
-            >
+            <FormControl isInvalid={errors.content} marginBottom={errors.content ? "4px" : "0px"}>
               <FormLabel>Content</FormLabel>
               <Textarea
-                id='content'
+                id="content"
                 placeholder="Write content for the block..."
                 {...register("content")}
-                onChange={(e) => {
+                onChange={e => {
                   setValue("content", e.target.value);
                 }}
                 value={content}
                 height={editBoxHeight}
               />
-              <Box color="red">
-                {errors.content && errors.content.message}
-              </Box>
+              <Box color="red">{errors.content && errors.content.message}</Box>
             </FormControl>
-            
+
             <Box height="48px" paddingY="12px" color="red">
               {errors.request && errors.request.message}
             </Box>
@@ -247,35 +258,30 @@ const BlockFormInput: React.FC<Props> = ({id, onClose}) => {
             <FormLabel>Preview</FormLabel>
             <Box height={editBoxHeight} overflow="scroll" backgroundColor="#f5f5f5">
               <ReactMarkdown className={styles.reactMarkDown} remarkPlugins={[remarkGfm]}>
-              {content}
+                {content}
               </ReactMarkdown>
             </Box>
           </Box>
         </Stack>
       </Stack>
-      <HStack
-        width="100%"
-        bg="white"
-      >
+      <HStack width="100%" bg="white">
         <Button
-          onClick={() => { if (findMissingField(getValues())) submit(getValues()) }}
+          onClick={() => {
+            if (findMissingField(getValues())) submit(getValues());
+          }}
         >
           {id ? "Update" : "Submit"}
         </Button>
-        <Button onClick={cancel}>
-          Cancel
-        </Button>
-        <Spacer/>
-        {
-          id ? (
-            <Button bg="red.400" color="white" onClick={del}>
-              Delete
-            </Button>
-          ) : null
-        }
+        <Button onClick={cancel}>Cancel</Button>
+        <Spacer />
+        {id ? (
+          <Button bg="red.400" color="white" onClick={del}>
+            Delete
+          </Button>
+        ) : null}
       </HStack>
     </form>
-  )
-}
+  );
+};
 
 export default BlockFormInput;
--- src/components/tabs/admin/blocks/FormValues.tsx
+++ src/components/tabs/admin/blocks/FormValues.tsx
@@ -1,9 +1,12 @@
+export enum DisplayType {
+  WEB = "web",
+  MOBILE = "mobile",
+}
 
 export type BlockFormValues = {
-    hexathon: string;
-    title: string;
-    content: string;
-    slug: string;
-  };
-  
- 
+  hexathon: string;
+  title: string;
+  content: string;
+  slug: string;
+  display: DisplayType;
+};
