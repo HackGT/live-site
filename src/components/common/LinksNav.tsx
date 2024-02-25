7c5211b5a7974e62ba6ab8db614fb645eca64b70
Anupama1912 <anair325@gatech.edu>
2024-02-25 17:28:51 -0500

    centered quicklinks

--- src/components/common/LinksNav.tsx
+++ src/components/common/LinksNav.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, HStack } from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Center, Flex, HStack } from "@chakra-ui/react";
 
 type Props = {
   links: string[][];
@@ -13,20 +13,22 @@ const LinksNav: React.FC<Props> = (props: Props) => {
   }, []);
 
   return width > 768 ? (
-    <Box
+    <Flex
       boxShadow="0 1px 1px rgb(0 0 0 / 0.1)"
       padding={2}
       borderRadius={4}
       marginTop={2}
+      width="fit-content"
+      alignSelf="center"
     >
-      <Breadcrumb separator="">
+      <Breadcrumb separator="|">
         {props.links.map(link => (
           <BreadcrumbItem>
             <BreadcrumbLink href={link[1]}>{link[0]}</BreadcrumbLink>
           </BreadcrumbItem>
         ))}
       </Breadcrumb>
-    </Box>
+    </Flex>
   ) : (
     <HStack spacing="10px" overflow="scroll">
       <Box boxShadow="0 3px 3px rgb(0 0 0 / 0.1)" padding={2} borderRadius={4} marginTop={2}>
