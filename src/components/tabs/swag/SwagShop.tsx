75a932515e5f8cd83144b666e2e2ea4908ef5695
Anupama1912 <anair325@gatech.edu>
2024-09-25 20:41:43 -0400

    updated swag points for hackgt 11

--- src/components/tabs/swag/SwagShop.tsx
+++ src/components/tabs/swag/SwagShop.tsx
@@ -187,10 +187,10 @@ const SwagShop: React.FC = props => {
         <CreateSwagModal isOpen={createSwagModalIsOpen} onClose={closeCreateSwagModal} />
       )}
       <Flex flexDirection="column" alignItems="center">
-        <Text id="pointCategory">Tier 1 (200 Points Each)</Text>
-        {itemGrid(150, 200)}
-        <Text id="pointCategory">Tier 2 (150 Points Each)</Text>
-        {itemGrid(50, 150)}
+        <Text id="pointCategory">Tier 1 (450 Points Each)</Text>
+        {itemGrid(200, 450)}
+        <Text id="pointCategory">Tier 2 (200 Points Each)</Text>
+        {itemGrid(50, 200)}
         <Text id="pointCategory">Tier 3 (50 Points Each)</Text>
         {itemGrid(10, 50)}
         <Text id="pointCategory">Tier 4 (10 Points Each)</Text>
